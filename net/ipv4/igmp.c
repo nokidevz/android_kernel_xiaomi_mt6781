023a71c595f0b408d50cf3baf77177b514c7f358
Greg Kroah-Hartman <gregkh@google.com>
2021-08-27 18:38:12 +0200

    Revert "net: igmp: fix data-race in igmp_ifc_timer_expire()"
    This reverts commit fb5db3106036f4e21a63c0c6b08db4b4f18f157c which is
    commit 4a2b285e7e103d4d6c6ed3e5052a0ff74a5d7f15 upstream.
    
    It breaks the kernel abi (well the fix for this fix does), and is not
    needed for Android devices.
    
    Cc: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I1c59c60493d216f1671b110a948f38293a0925ee

--- net/ipv4/igmp.c
+++ net/ipv4/igmp.c
@@ -807,7 +807,6 @@ static void igmp_gq_timer_expire(struct timer_list *t)
 static void igmp_ifc_timer_expire(struct timer_list *t)
 {
 	struct in_device *in_dev = from_timer(in_dev, t, mr_ifc_timer);
-	u8 mr_ifc_count;
 
 	igmpv3_send_cr(in_dev);
 	if (in_dev->mr_ifc_count) {
