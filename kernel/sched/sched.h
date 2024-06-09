fd90e335389d0d1c1bbbe594c2c9a424901bbc4d
nokidevz <petreskinove1@gmail.com>
2024-06-09 15:25:00 +0200

    Revert "sched: Optimize __calc_delta()"
    This reverts commit 51a0cc609eda0d84c9b4450fd0ba548d8f7c3564.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -36,7 +36,6 @@
 #include <uapi/linux/sched/types.h>
 
 #include <linux/binfmts.h>
-#include <linux/bitops.h>
 #include <linux/blkdev.h>
 #include <linux/compat.h>
 #include <linux/context_tracking.h>
